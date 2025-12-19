e1bd80b9db027de71f34e1177b0db773b074ee63
memoryleak47 <memoryleak47@protonmail.ch>
2025-12-19 23:27:21 +0100

    ...

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use egg::*;
+
+struct DetourCost;
+
+impl<L: Language> Analysis<L> for DetourCost {
+    type Data = (/*Cost*/usize, /*Ctxt Cost*/usize);
+
+    fn make(eg: &mut EGraph<L, Self>, n: &L, i: Id) -> Self::Data {
+        let cost = AstSize.cost(n, |i| eg[i].data.0);
+        (cost, 0)
+    }
+
+    fn merge(&mut self, d1: &mut Self::Data, d2: Self::Data) -> DidMerge {
+        d1.0 = d1.0.min(d2.0);
+        DidMerge(false, false)
+    }
+}
+
+define_language! {
+    pub enum Math {
+        "+" = Add([Id; 2]),
+        "-" = Sub([Id; 2]),
+        "*" = Mul([Id; 2]),
+        "/" = Div([Id; 2]),
+        Symbol(Symbol),
+    }
+}
+
+fn rules() -> Vec<Rewrite<Math, DetourCost>> {
+    vec![
+        rewrite!("cancel"; "(- ?a ?a)" => "z")
+    ]
+}
+
+fn main() {
+    let st = "(+ a (- b b))".parse().unwrap();
+    let r: Runner<Math, DetourCost> =
+        Runner::new(DetourCost)
+            .with_expr(&st)
+            .run(&rules());
+    let ex = Extractor::new(&r.egraph, AstSize);
+    println!("{}", ex.find_best(r.roots[0]).1);
+}
