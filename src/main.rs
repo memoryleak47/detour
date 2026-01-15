8e204977d865eb14314e5dd4d508ac31f48bab5d
memoryleak47 <memoryleak47@protonmail.ch>
2026-01-15 23:22:38 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,21 @@ use egg::*;
 
 struct DetourAnalysis;
 
-struct DetourScheduler;
-
-impl<L: Language> RewriteScheduler<L, DetourAnalysis> for DetourScheduler {
-    fn search_rewrite<'a>(&mut self, it: usize, eg: &EGraph<L, DetourAnalysis>, rw: &'a Rewrite<L, DetourAnalysis>) -> Vec<SearchMatches<'a, L>> {
-        let mut out = Vec::new();
-        let mut threshold = usize::MAX;
-        for e in eg.classes() {
-            let c = e.data;
-            let detour_cost = c.0 + c.1;
-            if detour_cost <= threshold {
-                let new: Option<SearchMatches<L>> = rw.searcher.search_eclass(eg, e.id);
-                if new.is_some() {
-                    if detour_cost < threshold {
-                        threshold = detour_cost;
-                        out.clear();
-                    }
-                    out.extend(new);
-                }
-            }
-        }
-        out
+// one iteration of eqsat governed by the detour system.
+fn detour_iter<L: Language>(rws: &[Rewrite<L, DetourAnalysis>], eg: &mut EGraph<L, DetourAnalysis>) {
+    let mut matches: Vec<_> = Vec::new();
+    for rw in rws {
+        let s = rw.searcher.search(eg);
+        matches.push(s);
     }
+
+    for (rw, matches) in rws.iter().zip(matches.into_iter()) {
+        rw.applier.apply_matches(eg, &matches, rw.name);
+    }
+
+    // TODO actually use the detour cost.
+
+    eg.rebuild();
 }
 
 impl<L: Language> Analysis<L> for DetourAnalysis {
@@ -31,7 +24,7 @@ impl<L: Language> Analysis<L> for DetourAnalysis {
 
     fn make(eg: &mut EGraph<L, Self>, n: &L, i: Id) -> Self::Data {
         let cost = AstSize.cost(n, |i| eg[i].data.0);
-        (cost, 0)
+        (cost, 0 /* TODO */)
     }
 
     fn merge(&mut self, d1: &mut Self::Data, d2: Self::Data) -> DidMerge {
@@ -70,13 +63,23 @@ fn rules() -> Vec<Rewrite<Math, DetourAnalysis>> {
     ]
 }
 
+fn init_term() -> String {
+    let mut s = String::from("zero");
+    for i in 0..5 {
+        s = format!("(+ (+ a{i} {s}) (- a{i}))");
+    }
+    s
+}
+
 fn main() {
-    let st = "(+ (+ b a) (- b))".parse().unwrap();
-    let r: Runner<Math, DetourAnalysis> =
-        Runner::new(DetourAnalysis)
-            .with_expr(&st)
-            .with_scheduler(DetourScheduler)
-            .run(&rules());
-    let ex = Extractor::new(&r.egraph, AstSize);
-    println!("{}", ex.find_best(r.roots[0]).1);
+    let st: RecExpr<Math> = init_term().parse().unwrap();
+    println!("Initial: {st}");
+    let mut eg = EGraph::new(DetourAnalysis);
+    let i = eg.add_expr(&st);
+    let rws = rules();
+    for _ in 0..5 {
+        detour_iter(&rws, &mut eg);
+    }
+    let ex = Extractor::new(&eg, AstSize);
+    println!("Extracted: {}", ex.find_best(i).1);
 }
